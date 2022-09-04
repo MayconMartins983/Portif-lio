40355ba69f9adb6682754d2b8b0be5290c4b01b1
Maycon Martins <maycon.rocha2100@gmail.com>
2022-09-04 17:11:41 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const buttonPort = document.getElementById('button-port') //seleciona o botão da section main através da manipulação do DOM
+const buttonContato = document.getElementById("button-contato") //seleciona o botão da section main através da manipulação do DOM
+
+
+buttonPort.addEventListener('click', () => { // dispara um evento ao clicar, fazendo com que a pagina vá para o elemento do DOM com o Id portifolio.
+    window.location.href = "#portifolio"
+});
+
+buttonContato.addEventListener('click', () => { // dispara um evento ao clicar, fazendo com que a pagina vá para o elemento do DOM com o Id contato.
+    window.location.href = "#contato"
+});
