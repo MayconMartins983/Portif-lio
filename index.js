187acb779f9479ebf2c74794e847ca340d17d2c6
Maycon Martins <maycon.rocha2100@gmail.com>
2022-09-04 18:00:53 -0300

    Adiciona modal

--- index.js
+++ index.js
@@ -5,7 +5,3 @@ const buttonContato = document.getElementById("button-contato") //seleciona o bo
 buttonPort.addEventListener('click', () => { // dispara um evento ao clicar, fazendo com que a pagina vá para o elemento do DOM com o Id portifolio.
     window.location.href = "#portifolio"
 });
-
-buttonContato.addEventListener('click', () => { // dispara um evento ao clicar, fazendo com que a pagina vá para o elemento do DOM com o Id contato.
-    window.location.href = "#contato"
-});
